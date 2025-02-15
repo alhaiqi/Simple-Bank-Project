1112116878a92bd0ca20f87e7da9366f24a2f864
alhaiqi <m.03edhigos@gmail.com>
2025-02-15 08:56:54 +0200

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+//New
+#include <iostream>
+#include "clsLoginScreen.h"
+
+int main()
+
+{
+    while (true)        
+    {
+        if (!clsLoginScreen::ShowLoginScreen())
+        {
+            break;
+        }
+
+    }
+
+    return 0;
+}
